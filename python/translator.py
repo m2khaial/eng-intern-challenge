c732087283ab0def95d1d74c16936abbbe11406b
Mai Khaial <m2khaial@uwaterloo.ca>
2024-08-31 10:56:45 +0000

    Created code framework

--- python/translator.py
+++ python/translator.py
@@ -1,1 +1,12 @@
+#Get input string from the test file
+#Determine if string is in english or braille (will 
+# determine if you need which translator to use)
+    #Create a translator class to handle the input language
+    #  and the language to translate to
+#Determine if string is in english or braille (will 
 
+#Covert to either english or braille based on the input 
+# language
+    #Create a class that handles braille to english
+    #Create a class that handles english to braille
+#output translated string into terminal
